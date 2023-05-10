8fd0bb15bd8fe0d60144154cc4fb0ec15b54df8f
ahmad <heydarpanah92>
2023-05-10 21:55:41 +0330

    nothing

--- assemble_truss_stiffness_matrix.py
+++ assemble_truss_stiffness_matrix.py
@@ -9,13 +9,10 @@ def assembleGlobalStiffnessMatrix(nodes, elements):
         degrees = element.degrees
         for i in degrees:
             for j in degrees:
-                i_in_global = i - 1
-                j_in_global = j - 1
+                i_in_global, j_in_global = i - 1, j - 1
+                l = list(degrees)
+                i_in_local, j_in_local = l.index(i), l.index(j)
 
-                i_in_local = list(degrees).index(i)
-                j_in_local = list(degrees).index(j)
-                # print(element.id, element.degrees, i, j,  i_in_global,
-                #   j_in_global, i_in_local, j_in_local)
                 K[i_in_global, j_in_global] = K[i_in_global, j_in_global] + \
                     element.globalK()[i_in_local, j_in_local]
 
--- main.py
+++ main.py
@@ -3,8 +3,11 @@ from plotter import plot_truss
 from read_json_input import get_elements, get_nodes, get_supports
 from models.node_model import Node
 import numpy as np
+import time
 
-input = 'input1.json'
+input = 'example_2.json'
+
+start_time = time.time()
 
 nodes = get_nodes(input)
 elements = get_elements(input, nodes)
@@ -12,7 +15,12 @@ supports = get_supports(input, nodes)
 
 
 stiffnessMatrix = assembleGlobalStiffnessMatrix(nodes, elements)
+
+end_time = time.time()
+
 np.savetxt('output.txt', stiffnessMatrix, fmt="%.3f")
+print('time:', end_time - start_time)
+print("+++++++++++++++ check output.txt for results +++++++++++++++++")
 
 
 plot_truss(elements, supports, nodes)
